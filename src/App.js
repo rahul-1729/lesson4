85bbd1418f32dd09987afe5b57cc2d3be82e6edf
rahul-1729 <rk9763981@gmail.com>
2024-09-16 12:04:32 +0530

    learned transformation

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ function App() {
           <div className='h-[400px] w-[400px] sm:h-[500px] sm:w-[500px] md:h-[650px] md:w-[650px] bg-gradient-to-l from-indigo-500  via-sky-400    to-emrald-500 rounded-full absolute bottom-2 -right-40 blur-sm'  ></div>
           <div className=' bg-gradient-to-r from-indigo-700 via-purple-700 to-pink-300 via h-[200px] w-[200px]   rounded-full absolute top-2 -left-32 blur-lg'  ></div>
           
+
+          <div className='  border-4 border-sky-500  md:h-[200px] md:w-[200px]   origin-top-left rotate-45  absolute top-1/3  right-1/2 blur-sm'  ></div>
+           
+          
            <Card/>
          
            
